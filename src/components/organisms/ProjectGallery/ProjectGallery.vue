345122be2a6030ba9c16a4cd31f4e80760146a75
João Rodrigues <joaobruno.rf@gmail.com>
2023-08-08 23:29:16 -0300

    feat: Create ProjectGallery and Item

---
+++ src/components/organisms/ProjectGallery/ProjectGallery.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup>
+import ProjectItem from "@/components/organisms/ProjectItem/ProjectItem.vue";
+</script>
+
+<template>
+  <div
+    class="portfolio-section"
+    style="
+      width: 100%;
+      padding-left: 80px;
+      padding-right: 80px;
+      padding-bottom: 80px;
+      background: #eeeeee;
+      justify-content: space-between;
+      display: flex;
+      flex-direction: column;
+    "
+  >
+    <div
+      style="
+        width: 100%;
+        justify-content: space-between;
+        display: flex;
+        flex-direction: row;
+        padding-bottom: 150px;
+      "
+    >
+      <ProjectItem
+        style="width: 60%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/866x635"
+      ></ProjectItem>
+      <ProjectItem
+        style="width: 30%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/433x635"
+      ></ProjectItem>
+    </div>
+    <div
+      style="
+        width: 100%;
+        justify-content: space-between;
+        display: flex;
+        flex-direction: row;
+        padding-bottom: 150px;
+      "
+    >
+      <ProjectItem
+        style="width: 30%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/433x635"
+      ></ProjectItem>
+      <ProjectItem
+        style="width: 60%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/866x635"
+      ></ProjectItem>
+    </div>
+    <div
+      style="
+        width: 100%;
+        justify-content: space-between;
+        display: flex;
+        flex-direction: row;
+        padding-bottom: 150px;
+      "
+    >
+      <ProjectItem
+        style="width: 60%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/866x635"
+      ></ProjectItem>
+      <ProjectItem
+        style="width: 30%; height: 635px"
+        projectName="Lorem Ipsum"
+        projectStacks="is simply dummy tex"
+        img="https://via.placeholder.com/433x635"
+      ></ProjectItem>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/pages/HomeView/HomeView.vue
+++ src/components/pages/HomeView/HomeView.vue
@@ -2,12 +2,14 @@
   <HeaderHero></HeaderHero>
   <HeroGreeting></HeroGreeting>
   <FooterHero></FooterHero>
+  <ProjectGalley></ProjectGalley>
 </template>
 
 <script lang="ts" setup>
 import HeaderHero from "@/components/molecules/HeaderHero/HeaderHero.vue";
 import HeroGreeting from "@/components/atoms/HeroGreeting/HeroGreeting.vue";
 import FooterHero from "@/components/atoms/FooterHero/FooterHero.vue";
+import ProjectGalley from "@/components/organisms/ProjectGallery/ProjectGallery.vue";
 </script>
 
 <style scoped></style>
